adefbcdd5dba90225b0e20979f76aa547c7e39bc
Tamer Berat Çelik <aducat5@hotmail.com>
2019-08-29 00:02:12 +0300

    added create character page and controller

---
+++ DnDWorld.PL.WEB/Controllers/CharacterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DnDWorld.PL.WEB.Controllers
+{
+    public class CharacterController : Controller
+    {
+        // GET: Character
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
