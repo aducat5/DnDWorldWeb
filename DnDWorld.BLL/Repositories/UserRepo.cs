fa408d6fd02bea923b68e69f7ce377b60d890c06
Tamer Berat Çelik <aducat5@hotmail.com>
2019-09-05 09:44:22 +0300

    develop on user repo

--- DnDWorld.BLL/Repositories/UserRepo.cs
+++ DnDWorld.BLL/Repositories/UserRepo.cs
@@ -8,12 +8,12 @@ namespace DnDWorld.BLL.Repositories
     public class UserRepo
     {
         private static DnDWorldDBEntities db = DBTools.DBInstance;
-        public bool DoesUserExists(int id) => db.Users.Any(u => u.UserID == id);
-        public bool DoesUserExists(string email) => db.Users.Any(u => u.Email == email);
+        public bool CheckUser(int id) => db.Users.Any(u => u.UserID == id);
+        public bool CheckUser(string email) => db.Users.Any(u => u.Email == email);
         public User GetUser(int id)
         {
             User user = null;
-            if (DoesUserExists(id)) user = db.Users.Find(id);
+            if (CheckUser(id)) user = db.Users.Find(id);
             return user;
         }
         public User GetUser(string email, string password)
@@ -27,7 +27,7 @@ namespace DnDWorld.BLL.Repositories
             newUser.UpdateDate = newUser.CreateDate;
             try
             {
-                if (DoesUserExists(newUser.Email))
+                if (CheckUser(newUser.Email))
                 {
                     islemSonucu = "Bu email zaten kullanımda.";
                     return false;
@@ -65,6 +65,45 @@ namespace DnDWorld.BLL.Repositories
                 p.ContentID == contentID &&
                 p.GrantedUserID == userID
             );
+        }
+
+        public bool DeleteUser(int id, out string islemSonucu)
+        {
+            bool sonuc = false;
+            if (CheckUser(id))
+            {
+                User user = GetUser(id);
+                user.IsDeleted = true;
+                try
+                {
+                    sonuc = db.SaveChanges() > 0;
+                    if (sonuc) islemSonucu = "Silme işlemi başarılı.";
+                    else islemSonucu = "Bir hata oluştu.";
+                }
+                catch (Exception ex)
+                {
+                    islemSonucu = ex.Message;
+                }
+            }
+            else islemSonucu = "Silinmek istenen kullancı bulunamadı.";
+            return sonuc;
+        }
+        public bool UpdateUser(User user, out string islemSonucu)
+        {
+            bool sonuc = false;
+            if (user != null && user.Fullname.IsUsable())
+            {
+                if (CheckUser(user.UserID))
+                {
+                    user.UpdateDate = DateTime.Now;
+                    db.Entry(GetUser(user.UserID)).CurrentValues.SetValues(user);
+                    if (db.SaveChanges() > 0) islemSonucu = "Güncelleme başarılı";
+                    else islemSonucu = "Bir hata oluştu";
+                }
+                else islemSonucu = "Kullanıcı Bulunamadı.";
+            }
+            else islemSonucu = "Kullanıcı nesnesi boş olamaz";
+            return sonuc;
         }
     }
 }
--- DnDWorld.BLL/Utility/GenFunx.cs
+++ DnDWorld.BLL/Utility/GenFunx.cs
@@ -4,19 +4,48 @@ namespace DnDWorld.BLL.Utility
 {
     public static class GenFunx
     {
+        /// <summary>
+        /// Converts enum to int value
+        /// </summary>
+        /// <param name="enumValue"></param>
+        /// <returns></returns>
         public static int ToInt(this Enum enumValue)
         {
             return Convert.ToInt32(enumValue);
         }
 
+        /// <summary>
+        /// Converts string to int, returns 0 if can't convert.
+        /// </summary>
+        /// <param name="stringValue"></param>
+        /// <returns></returns>
         public static int ToInt(this string stringValue)
         {
-            return Convert.ToInt32(stringValue);
+            int val = 0;
+            try
+            {
+                val = Convert.ToInt32(stringValue);
+            }
+            catch (Exception)
+            {
+
+            }
+            return val;
         }
 
+        /// <summary>
+        /// Clears the text from whitespace, dots, semi-colons, ampersants, asterikses and slashes
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
         public static string ClearText(this string text)
         {
-            return text.Trim(' ', '.', ';', '&', '*', '/');
+            return text.Trim(' ', '.', ';', '&', '*', '/', '\\');
+        }
+
+        public static bool IsUsable(this string text)
+        {
+            return (!string.IsNullOrEmpty(text) && !string.IsNullOrWhiteSpace(text) && !text.Contains(".") && !text.Contains(";") && !text.Contains("&") && !text.Contains("*") && !text.Contains("/") && !text.Contains("\\"));
         }
     }
 }
