6eb5f704e5cd5ecc6f6cc290bacb6a8e514daaa8
Tamer Berat Çelik <aducat5@hotmail.com>
2019-09-12 21:27:23 +0300

    added classes to character creation page

---
+++ DnDWorld.BLL/Repositories/ClassRepo.cs
@@ -0,0 +1,20 @@
+﻿using DnDWorld.BLL.Utility;
+using DnDWorld.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDWorld.BLL.Repositories
+{
+    public class ClassRepo
+    {
+        DNDWDBEntities db = DBTools.DBInstance;
+
+        public List<Class> GetClasses()
+        {
+            return db.Classes.ToList();
+        }
+    }
+}
---
+++ DnDWorld.PL.WEB/Controllers/ClassController.cs
@@ -0,0 +1,34 @@
+﻿using DnDWorld.BLL.Repositories;
+using DnDWorld.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DnDWorld.PL.WEB.Controllers
+{
+    public class ClassController : Controller
+    {
+        ClassRepo cr = new ClassRepo();
+        public JsonResult GetClasses()
+        {
+            List<Class> classes = cr.GetClasses();
+            List<Class> classDTO = new List<Class>();
+            if (classes != null)
+            {
+                foreach (Class c in classes)
+                {
+                    classDTO.Add(new Class()
+                    {
+                        Fullname = c.Fullname,
+                        PicturePath = c.PicturePath,
+                        ClassID = c.ClassID,
+                        Description = c.Description
+                    });
+                }
+            }
+            return Json(classes, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
