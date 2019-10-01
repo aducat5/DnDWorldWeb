d88836aea716d80bd8a1d8d00df9b986b574fad7
Tamer Berat Çelik <aducat5@hotmail.com>
2019-10-01 15:57:51 +0300

    campaign create page is working but needs exception handling and security imporvements

---
+++ DnDWorld.BLL/Repositories/CampaignRepo.cs
@@ -0,0 +1,45 @@
+﻿using DnDWorld.BLL.Utility;
+using DnDWorld.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDWorld.BLL.Repositories
+{
+    public class CampaignRepo
+    {
+        DNDWDBEntities db = DBTools.DBInstance;
+        public bool DoesCampaignExists(string charName, int userID)
+        {
+            return db.Campaigns.Any(c => c.Fullname == charName && c.OwnerID == userID);
+        }
+
+        public bool InsertCampaign(Campaign newCamp, out string islemSonucu)
+        {
+            newCamp.Fullname = newCamp.Fullname.ClearText();
+            newCamp.IsDeleted = false;
+            newCamp.IsOpen = false;
+            if (!DoesCampaignExists(newCamp.Fullname, newCamp.OwnerID))
+            {
+                db.Campaigns.Add(newCamp);
+                if (db.SaveChanges() > 0)
+                {
+                    islemSonucu = "Başarılı";
+                    return true;
+                }
+                else
+                {
+                    islemSonucu = "Bir hata oluştu";
+                    return false;
+                }
+            }
+            else
+            {
+                islemSonucu = "Böyle bir maceraya zaten sahipsin";
+                return false;
+            }
+        }
+    }
+}
--- DnDWorld.PL.WEB/Controllers/CampaignController.cs
+++ DnDWorld.PL.WEB/Controllers/CampaignController.cs
@@ -1,4 +1,6 @@
-﻿using DnDWorld.BSL.Authorization;
+﻿using DnDWorld.BLL.Repositories;
+using DnDWorld.BLL.Utility;
+using DnDWorld.BSL.Authorization;
 using DnDWorld.DAL;
 using System;
 using System.Collections.Generic;
@@ -10,6 +12,7 @@ namespace DnDWorld.PL.WEB.Controllers
 {
     public class CampaignController : Controller
     {
+        CampaignRepo cr = new CampaignRepo();
         [UserAuth]
         public ActionResult Create()
         {
@@ -17,9 +20,37 @@ namespace DnDWorld.PL.WEB.Controllers
         }
 
         [UserAuth, HttpPost]
-        public ActionResult Create(Campaign newCampaign)
+        public ActionResult Create(string universeID, string planetID, string txtCampaignName, bool chkIsPublic = false)
         {
-            return View();
+            int uniID = universeID.ToInt();
+            int plaID = planetID.ToInt();
+
+
+            User current = Session["user"] as User;
+
+            Campaign newCampaign = new Campaign()
+            {
+                CreateDate = DateTime.Now,
+                Fullname = txtCampaignName.ClearText(),
+                IsOpen = false,
+                IsDeleted = false,
+                UpdateDate = DateTime.Now,
+                UniverseID = uniID,
+                PlanetID = plaID,
+                OwnerID = current.UserID
+            };
+
+            bool sonuc = cr.InsertCampaign(newCampaign, out string islemSonucu);
+            if (sonuc)
+            {
+                return RedirectToAction("ViewProfile", "Profile");
+            }
+            else
+            {
+                ViewBag.AlertMessage = islemSonucu;
+                ViewBag.AlertClass = "alert alert-danger";
+                return View();
+            }
         }
 
     }
