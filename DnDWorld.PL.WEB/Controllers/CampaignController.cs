6653a280b9171e3b5d6f9ce5095593c225c3b332
Tamer Berat Çelik <aducat5@hotmail.com>
2019-10-01 15:24:07 +0300

    created campaign creation page

---
+++ DnDWorld.PL.WEB/Controllers/CampaignController.cs
@@ -0,0 +1,26 @@
+﻿using DnDWorld.BSL.Authorization;
+using DnDWorld.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DnDWorld.PL.WEB.Controllers
+{
+    public class CampaignController : Controller
+    {
+        [UserAuth]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [UserAuth, HttpPost]
+        public ActionResult Create(Campaign newCampaign)
+        {
+            return View();
+        }
+
+    }
+}
